f6d979b37dea87ccfec5e22ff08d83ff7f966680
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2018-06-12 11:56:38 +0200

    refactor server and import modular schema
    This will clean up src/index.ts so it just has the server and it imports the schema from the schema directory

--- src/index.ts
+++ src/index.ts
@@ -1,126 +1,14 @@
-import { GraphQLServer } from 'graphql-yoga'
-import { importSchema } from 'graphql-import'
-import { Prisma } from './generated/prisma'
-import { Context } from './utils'
-
-const resolvers = {
-  Query: {
-    music(parent, args, context: Context, info) {
-      return context.db.query.songs({ }, info)
-    },
-  },
-  Mutation: {
-    async createSong(parent, { input }, context: Context, info) {
-      const { album, artist, featuring, title } = input;
-
-      const albumExists = await context.db.exists.Album({
-        alias: album.alias,
-      });
-
-      const artistExists = await context.db.exists.Artist({
-        alias: artist.alias,
-      });
-
-      const featuringExists = await context.db.exists.Artist({
-        alias: featuring ? featuring.alias : '',
-      });
-      
-      /**
-       * TODO: Refactor this for scenario where either album or artist is true
-       */
-
-      if (albumExists && artistExists && featuringExists) {
-        return context.db.mutation.createSong({
-          data: {
-            album: {
-              connect: { alias: album.alias },
-            },
-            artist: {
-              connect: { alias: artist.alias },
-            },
-            featuring: {
-              connect: { alias: featuring.alias },
-            },
-            title,
-          }
-        }, info);
-      }
-
-      if (albumExists && artistExists) {
-        return context.db.mutation.createSong({
-          data: {
-            album: {
-              connect: { alias: album.alias },
-            },
-            artist: {
-              connect: { alias: artist.alias },
-            },
-            title,
-          }
-        }, info);
-      }
-
-      const createdArtist = await context.db.mutation.createArtist({ 
-        data: {
-          alias: artist.alias,
-          name: artist.name,
-        },
-      }, info);
-
-      const createdAlbum = await context.db.mutation.createAlbum({ 
-        data: {
-          alias: album.alias,
-          artist: {
-            connect: {
-              id: createdArtist.id,
-            },
-          },
-          name: album.name,
-        },
-      }, info);
-
-      /**
-       * TODO: Refactor to handle creation of more that one artist feature
-       */
-      const createdFeature = await context.db.mutation.createArtist({ 
-        data: {
-          alias: featuring.alias,
-          name: featuring.name,
-        },
-      }, info);
-
-      return context.db.mutation.createSong({
-        data: {
-          album: {
-            connect: { 
-              id: createdAlbum.id,
-            },
-          },
-          artist: {
-            connect: {
-              id: createdArtist.id,
-            },
-          },
-          featuring: {
-            connect: {
-              id: createdFeature.id,
-            },
-          },
-          title,
-        }
-      }, info);
-    }
-  },
-}
+import { GraphQLServer } from 'graphql-yoga';
+import { Prisma } from './generated/prisma';
+import * as schema from './schema';
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  resolvers,
+  ...schema,
   context: req => ({
     ...req,
     db: new Prisma({
-      endpoint: 'https://eu1.prisma.sh/elandamor/pdPrisma/dev', // the endpoint of the Prisma API
-      debug: true, // log all GraphQL queries & mutations sent to the Prisma API
+      endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma API
+      debug: Boolean(process.env.NODE_ENV === 'production'), // log all GraphQL queries & mutations sent to the Prisma API
       // secret: 'mysecret123', // only needed if specified in `database/prisma.yml`
     }),
   }),
